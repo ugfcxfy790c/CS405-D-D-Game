c73ea51bb67aa974bf7af55f10196dab9c4bb377
EliDOVX <edorbach43@gmail.com>
2024-02-20 20:36:09 -0500

    Update Enemy.java

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Enemy {
     
 public static void main(String[] args) {
@@ -10,14 +12,21 @@ public static void main(String[] args) {
     private int health;
     private int atk;
     private int maxDmg;
+    private int aC;
     
-    public Enemy(String EType, int difficulty, double[] res, int health, int atk, int maxDmg) {
+    public Enemy(String EType, int difficulty, double[] res, int health, int atk, int maxDmg, int aC) {
         this.EType = EType;
         this.difficulty = difficulty;
         this.res = res;
         this.health = health;
         this.atk = atk;
         this.maxDmg = maxDmg;
+        this.aC = aC;
+    }
+
+    public static int diceRoller(int nSides) {
+        Random dice = new Random();
+        return dice.nextInt(nSides) + 1;
     }
 
     public String getEType() {
@@ -28,6 +37,7 @@ public static void main(String[] args) {
         return this.difficulty;
     }
     
+    //Returns the proper resistance multiplier for a given DamageType enum
     public double getRes(DamageType type) {
         if (type == DamageType.BLUDGEONING) {
             return this.res[0];
@@ -60,5 +70,30 @@ public static void main(String[] args) {
 
     public int getMaxDmg() {
         return this.maxDmg;
+    }
+
+    public int getAC() {
+        return this.aC;
+    }
+
+    public boolean eAttack(int roll) {
+        return (roll >= this.aC);
+    }
+
+    public void eDamage(int damage) {
+        this.health -= damage;
+    }
+
+    //Makes an attack roll and does damage to the player if neccesary
+    public int dmgPlayer(int aC) {
+        int roll = diceRoller(20) + atk;
+        if (roll >= aC) {
+            return dmgRollPlayer();
+        }
+        return 0;
+    }
+
+    private int dmgRollPlayer() {
+       return diceRoller(this.maxDmg);
     }
 }
