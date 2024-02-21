306dc2a8a687c0213c414c00e55f874b2d428fd4
EliDOVX <edorbach43@gmail.com>
2024-02-20 19:16:47 -0500

    Update Enemy.java

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,5 +1,25 @@
 public class Enemy {
     
-    private String type;
-    private String difficu
+public static void main(String[] args) {
+    System.out.println();
+}
+
+    private String Type;
+    private int difficulty;
+    private double[] resistances;
+    private int health;
+    private int atk;
+    private int maxDmg;
+    
+    public Enemy(String Type, int difficulty, double[] resistances, int health, int atk, int maxDmg) {
+        this.Type = Type;
+        this.difficulty = difficulty;
+        this.resistances = resistances;
+        this.health = health;
+        this.atk = atk;
+        this.maxDmg = maxDmg;
+    }
+
+
+        
 }
