89782cd7b65186c0b4be81bcda38bb92b58581fd
han-z-yan <149729250+han-z-yan@users.noreply.github.com>
2024-02-22 16:23:26 -0500

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -68,6 +68,28 @@ public class Player {
         return 1.0;
     }
 
+    public void setRes(DamageType type, double additor) {
+        if (type == DamageType.BLUDGEONING) {
+            this.res[0] -= additor;
+        }
+        else if (type == DamageType.PIERCING) {
+            this.res[1] -= additor;
+        }
+        else if (type == DamageType.SLASHING) {
+            this.res[2] -= additor;
+        }
+        else if (type == DamageType.FIRE) {
+            this.res[3] -= additor;
+        }
+        else if (type == DamageType.ELECTRICITY) {
+            this.res[4] -= additor;
+        }
+        else if (type == DamageType.POISON) {
+            this.res[5] -= additor;
+        }
+    }
+
+
     public int damageToEnemy(Enemy enemy) {
         double enemyHealth = enemy.getHealth();
         double enemyResistance = enemy.getRes(DamageType.BLUDGEONING);
