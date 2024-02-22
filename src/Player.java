8254383fa47702d4ec079111c8f3bb075a3062a5
han-z-yan <149729250+han-z-yan@users.noreply.github.com>
2024-02-22 16:05:09 -0500

    Player edits

--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,8 @@
 //Hello 
 
-public class Player{
+import java.util.Random;
+
+public class Player {
 
     private double health;
     private Weapon[] weapons;
@@ -9,18 +11,38 @@ public class Player{
     private int atk;
     private double[] resistances;
 
+    Player player = new Player(100, 15, 5);
+
     public Player(double health, int aC, int atk) {
         this.health = health;
         this.aC = aC;
         this.atk = atk;
-        this.resistances = new double[] {1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+        this.resistances = new double[] { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 };
+    }
+
+    public Player getPlayer(){
+        return this.player;
     }
 
     public int getAC() {
         return this.aC;
     }
 
+    public static int diceRoller(int nSides) {
+        Random dice = new Random();
+        return dice.nextInt(nSides) + 1;
+    }
+
     public int getAtk() {
         return this.atk;
     }
+
+    public int damageToEnemy(Enemy enemy) {
+        return 
+    }
+
+    public void damageToPlayer(double damage) {
+        this.health -= damage;
+    }
+
 }
