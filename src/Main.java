f949fff6d686f1e1a82f82ee5f21e82bb3290fc6
ugfcxfy790c <tiz5mim3@duck.com>
2024-02-24 16:34:11 -0500

    initialized all weapons

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         int gameLength = 30;
         for (int i = 0; i < gameLength; i++) {
             newRoom(i + 1, player);
-        }   
+        }
         System.out.println("You win!");
     }
 
@@ -17,29 +17,32 @@ public class Main {
         player.updateInventory();
         fight(enemyList(Math.sqrt(roomNumber)), player);
 
+        Item ite = Item.spawn(player);
+        player.addToInventory(ite);
         rewards(player);
+
 
     }
 
     public static Enemy[] enemyList(double difficulty) {
-        Enemy e1 = new Enemy("Goblin", difficulty, new double[] {1, 1, 1, 1, 1, 1, 1}, 13, DamageType.PIERCING);
-        Enemy e2 = new Enemy("Cultist", difficulty, new double[] {1, 1.5, 1, 0, 1, 1, 1.5}, 10, DamageType.SLASHING);
-        Enemy e3 = new Enemy("Skeleton", difficulty, new double[] {5, 0, 0.5, 1, 0.5, 0, 1}, 12, DamageType.BLUDGEONING);
-        Enemy e4 = new Enemy("Dragon Goddess Tiamat", difficulty, new double[] {1, 1.5, 1, 0, 0, 0, 1}, 18, DamageType.FIRE);
-        Enemy e5 = new Enemy("Beholder", difficulty, new double[] {0.5, 1.5, 0.5, 1, 2, 1, 0}, 15, DamageType.ELECTRICITY);
-        Enemy e6 = new Enemy("Obirith", difficulty, new double[] {0, 1, 0, 1.5, 1, 2, 1.5}, 14, DamageType.POISON);
-        Enemy e7 = new Enemy("Mad Wizard", difficulty, new double[] {Enemy.diceRoller(100)/70.0, Enemy.diceRoller(100)/70.0, Enemy.diceRoller(100)/70.0, Enemy.diceRoller(100)/70.0, Enemy.diceRoller(100)/70.0, Enemy.diceRoller(100)/70.0, Enemy.diceRoller(100) / 70}, 9, DamageType.FIRE);
-        Enemy e8 = new Enemy("Storm Giant", difficulty, new double[] {1, 2, 1, 1, 0, 0.5, 1}, 16, DamageType.ELECTRICITY);
-        Enemy e9 = new Enemy("Wraith", difficulty, new double[] {0.5, 1, 0, 1, 3, 0, 1}, 12, DamageType.POISON);
-        Enemy e10 = new Enemy("Red Slaad", difficulty, new double[] {1, 0.5, 0.75, 0, 2, 1.5, 0}, 12, DamageType.SLASHING);
-        Enemy e11 = new Enemy("Blue Slaad", difficulty, new double[] {1, 0.5, 0.75, 2, 0, 1.5, 0}, 12, DamageType.SLASHING);
-        Enemy e12 = new Enemy("Terminator", difficulty, new double[] {0.5, 1, 0, 1.5, 2.5, 0, 0}, 19, DamageType.BLUDGEONING);
-        Enemy e13 = new Enemy("Mr. Cosgrove", difficulty, new double[] {2, 2, 2, 2, 0, 2, -3}, 21, DamageType.POISON);
-        Enemy e14 = new Enemy("Sphinx", difficulty, new double[] {1, 1, 2, 0.25, 0.5, 1, 2}, 14, DamageType.PIERCING);
-        Enemy e15 = new Enemy("Atropal", difficulty, new double[] {0, 1.5, 0, 1, 0, 2, 0.75}, 8, DamageType.FIRE);
-        Enemy e16 = new Enemy("Baby Tarasque", difficulty, new double[] {0.5, 0.5, 0.5, 0.1, 5, 0.1, 1}, 15, DamageType.SLASHING);
+        Enemy e1 = new Enemy("Goblin", difficulty, new double[]{1, 1, 1, 1, 1, 1, 1}, 13, DamageType.PIERCING);
+        Enemy e2 = new Enemy("Cultist", difficulty, new double[]{1, 1.5, 1, 0, 1, 1, 1.5}, 10, DamageType.SLASHING);
+        Enemy e3 = new Enemy("Skeleton", difficulty, new double[]{5, 0, 0.5, 1, 0.5, 0, 1}, 12, DamageType.BLUDGEONING);
+        Enemy e4 = new Enemy("Dragon Goddess Tiamat", difficulty, new double[]{1, 1.5, 1, 0, 0, 0, 1}, 18, DamageType.FIRE);
+        Enemy e5 = new Enemy("Beholder", difficulty, new double[]{0.5, 1.5, 0.5, 1, 2, 1, 0}, 15, DamageType.ELECTRICITY);
+        Enemy e6 = new Enemy("Obirith", difficulty, new double[]{0, 1, 0, 1.5, 1, 2, 1.5}, 14, DamageType.POISON);
+        Enemy e7 = new Enemy("Mad Wizard", difficulty, new double[]{Enemy.diceRoller(100) / 70.0, Enemy.diceRoller(100) / 70.0, Enemy.diceRoller(100) / 70.0, Enemy.diceRoller(100) / 70.0, Enemy.diceRoller(100) / 70.0, Enemy.diceRoller(100) / 70.0, Enemy.diceRoller(100) / 70}, 9, DamageType.FIRE);
+        Enemy e8 = new Enemy("Storm Giant", difficulty, new double[]{1, 2, 1, 1, 0, 0.5, 1}, 16, DamageType.ELECTRICITY);
+        Enemy e9 = new Enemy("Wraith", difficulty, new double[]{0.5, 1, 0, 1, 3, 0, 1}, 12, DamageType.POISON);
+        Enemy e10 = new Enemy("Red Slaad", difficulty, new double[]{1, 0.5, 0.75, 0, 2, 1.5, 0}, 12, DamageType.SLASHING);
+        Enemy e11 = new Enemy("Blue Slaad", difficulty, new double[]{1, 0.5, 0.75, 2, 0, 1.5, 0}, 12, DamageType.SLASHING);
+        Enemy e12 = new Enemy("Terminator", difficulty, new double[]{0.5, 1, 0, 1.5, 2.5, 0, 0}, 19, DamageType.BLUDGEONING);
+        Enemy e13 = new Enemy("Mr. Cosgrove", difficulty, new double[]{2, 2, 2, 2, 0, 2, -3}, 21, DamageType.POISON);
+        Enemy e14 = new Enemy("Sphinx", difficulty, new double[]{1, 1, 2, 0.25, 0.5, 1, 2}, 14, DamageType.PIERCING);
+        Enemy e15 = new Enemy("Atropal", difficulty, new double[]{0, 1.5, 0, 1, 0, 2, 0.75}, 8, DamageType.FIRE);
+        Enemy e16 = new Enemy("Baby Tarasque", difficulty, new double[]{0.5, 0.5, 0.5, 0.1, 5, 0.1, 1}, 15, DamageType.SLASHING);
 
-        return new Enemy[] {e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15, e16};
+        return new Enemy[]{e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15, e16};
     }
 
     public static void fight(Enemy[] eList, Player player) {
@@ -48,21 +51,17 @@ public class Main {
         String entMessage;
         if (entry == 1) {
             entMessage = " emerges from the shadows!";
-        }
-        else if (entry == 2) {
+        } else if (entry == 2) {
             entMessage = " enters the room and screams!";
-        }
-        else if (entry == 3) {
+        } else if (entry == 3) {
             entMessage = " is released from storage into the room!";
-        }
-        else if (entry == 4) {
+        } else if (entry == 4) {
             entMessage = " drops down from the ceiling!";
-        }
-        else {
+        } else {
             entMessage = " pops into existence!";
         }
         System.out.println("A " + enemy.getEType() + entMessage);
-        while(player.getHealth() > 0 && enemy.getHealth() > 0) {
+        while (player.getHealth() > 0 && enemy.getHealth() > 0) {
             System.out.println("Your turn:");
             boolean running = true;
             while (running) {
@@ -73,8 +72,7 @@ public class Main {
                     System.out.println(player.showInventory());
                     if (player.getItemCount() == 0) {
                         System.out.println("You have no items.");
-                    }
-                    else {
+                    } else {
                         int itemSelect = input.nextInt();
                         input.nextLine();
                         Item item = player.getItem(itemSelect);
@@ -90,7 +88,7 @@ public class Main {
                     double dmg = player.damageToEnemy(enemy, weaponSelect);
                     System.out.println("You attack the " + enemy.getEType() + " with your " + player.getWeapon(weaponSelect).getName() + ", doing " + dmg + " damage.");
                     if (enemy.getEType() == "Mr. Cosgrove" && player.getWeapon(weaponSelect).getType() == DamageType.PSYCHIC) {
-                      System.out.println("The mental pain only seems to heal him!");
+                        System.out.println("The mental pain only seems to heal him!");
                     }
                     running = false;
                 }
@@ -98,23 +96,25 @@ public class Main {
 
             if (enemy.getHealth() >= 0) {
                 if (enemy.getHealth() >= 0) {
-                double damage = player.damageToPlayer(enemy.dmgPlayer(player.getAC()), enemy.getType());
-                System.out.println("The " + enemy.getEType() + " attacks, doing " + damage + " damage.");
-                System.out.println("You have " + player.getHealth() + " health left");
+                    double damage = player.damageToPlayer(enemy.dmgPlayer(player.getAC()), enemy.getType());
+                    System.out.println("The " + enemy.getEType() + " attacks, doing " + damage + " damage.");
+                    System.out.println("You have " + player.getHealth() + " health left");
+                }
             }
-        }
-        if (player.getHealth() <= 0) {
-            System.out.println("You are dead...");
-            System.exit(0);
-        }
-        if (enemy.getHealth() <= 0) {
-            System.out.println("The " + enemy.getEType() + " has been slain!");
-        }
+            if (player.getHealth() <= 0) {
+                System.out.println("You are dead...");
+                System.exit(0);
+            }
+            if (enemy.getHealth() <= 0) {
+                System.out.println("The " + enemy.getEType() + " has been slain!");
+            }
 
+        }
     }
 
-    public static void rewards(Player player) {
+    public static void rewards (Player player){
         System.out.println("\n" + "Choose a weapon to acquire/level up:");
 
     }
+
 }
